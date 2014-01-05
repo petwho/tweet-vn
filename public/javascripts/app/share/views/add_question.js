4fd25ac0f6a0e77cc0173589036d3877d4cdad73
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-05 23:42:38 +0700

    Add 'topic_id' into Modal topic input checkbox

--- public/javascripts/app/share/views/add_question.js
+++ public/javascripts/app/share/views/add_question.js
@@ -6,14 +6,17 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
       'click #add-question': 'addQuestion',
       'click #question-modal .next' : 'next',
       'click #question-modal .prev' : 'prev',
-      'keyup #question-modal .q-topics' : 'searchTopic'
+      'keyup #question-modal .search-input' : 'searchTopic'
     },
 
     initialize: function () {
       this.$modal           = $('#question-modal');
+      this.$titleBox        = $('#question-modal .title-box'),
       this.$questionTitle   = $('#question-modal .q-title');
-      this.$questionTopics  = $('#question-modal .q-topics');
       this.$notice          = $('#question-modal .notice');
+      this.$topicBox        = $('#question-modal .topic-box');
+      this.$searchInput     = $('#question-modal .search-input');
+      this.$suggestTopics   = $('.suggest-topics');
       this.$searchResults   = $('#question-modal .search-results');
 
       this.$cancel  = $('#question-modal .cancel');
@@ -22,6 +25,7 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
       this.$next    = $('#question-modal .next');
       this.$submit  = $('#question-modal .submit');
       this.timer    = null;
+      this.topics   = [];
 
       $('#question-modal form').on('submit', function (e) {
         e.preventDefault();
@@ -29,9 +33,8 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
     },
 
     setupView: function () {
-      this.$questionTitle.hide();
-      this.$questionTopics.hide();
-      this.$searchResults.hide();
+      this.$titleBox.hide();
+      this.$topicBox.hide();
       this.$notice.hide();
 
       this.$cancel.hide();
@@ -45,7 +48,7 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
 
       this.$cancel.show();
       this.$next.show();
-      this.$questionTitle.show();
+      this.$titleBox.show();
 
       this.$modal.modal();
       this.$questionTitle.focus();
@@ -58,20 +61,69 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
         this.$notice.show();
         return;
       }
-
+      this.suggestTopicsFunc();
+      spinner.start();
       this.setupView();
       this.$prev.show();
       this.$submit.show();
-      this.$questionTopics.show();
+      this.$topicBox.show();
+      if (this.$searchResults.find('ul > li').length === 0) {
+        this.$searchResults.hide();
+      }
+      this.$searchInput.focus();
     },
 
+    suggestTopicsFunc : function () {
+      var self = this;
+      $.ajax({
+        type  : 'GET',
+        url   : '/topics/list',
+        error : function (jqXHR, textStatus, errorThrow) {
+          spinner.stop();
+        },
+        success : function (topics, textStatus, jqXHR) {
+          var i, j, word, matched_list,
+            html          = '',
+            stat_list     = {},
+            topics_length = topics.length;
 
+          self.topics = topics;
+          self.$suggestTopics.empty();
+
+          for (i = 0; i < topics_length; i++) {
+            for (j = 0; j < topics[i].related_words.length; j++) {
+              word = topics[i].related_words[j];
+              if (stat_list[word] === undefined) {
+                stat_list[word] = 0;
+              }
+              matched_list = self.$questionTitle.val().match(new RegExp(word, 'gi'));
+              if (matched_list) {
+                stat_list[word] += matched_list.length;
+              }
+            }
+          }
+          Object.keys(stat_list).map(function (key) {
+            if (stat_list[key] !== 0) {
+              for (i = 0; i < topics_length; i++) {
+                if (topics[i].related_words.indexOf(key) !== -1) {
+                  self.$suggestTopics.append('<div class="checkbox"><label>'
+                    + '<input type="checkbox" name="topic" value="' + topics[i]._id + '" checked>'
+                    + '<span class="name">' + topics[i].name + '</span>'
+                    + '<span class="followers">' + topics[i].follower_count + ' followers</span>');
+                }
+              }
+            }
+          });
+          spinner.stop();
+        }
+      });
+    },
 
     prev : function () {
       this.setupView();
       this.$cancel.show();
       this.$next.show();
-      this.$questionTitle.show();
+      this.$titleBox.show();
       this.$questionTitle.focus();
     },
 
@@ -81,17 +133,18 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
       this.timer = setTimeout(this.searchTopicCallback(this), 100);
     },
 
-    clearSuggestTopics : function () {
+    clearSearchResults : function () {
       this.$searchResults.hide().find('ul').empty();
     },
 
     searchTopicCallback : function (that) {
       var self = that,
-        term = self.$questionTopics.val();
+        term = self.$searchInput.val();
 
-      self.clearSuggestTopics();
-
-      if (!term.trim()) { return; }
+      if (!term.trim()) {
+        self.clearSearchResults();
+        return;
+      }
 
       spinner.start();
       $.ajax({
@@ -104,6 +157,7 @@ define(['jquery', 'backbone', 'spinner'], function ($, Backbone, spinner) {
           var i,
             length = topics.length;
 
+          self.clearSearchResults();
           spinner.stop();
           if (length !== 0) {
             for (i = 0; i < length; i++) {
