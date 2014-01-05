8300f219d15991785d5b363f3220ed331d165f71
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-05 11:38:30 +0700

    - Add 'related_words' to Topic schema - Update 'filterInput' func

--- data/schemas/topic.js
+++ data/schemas/topic.js
@@ -6,6 +6,7 @@ TopicSchema = new Schema({
   name            : { type: String,   required: true, unique  : true },
   is_parent       : { type: Boolean,  required: true },
   parent          : { type: Schema.Types.ObjectId, ref: 'Topics', sparse: true },
+  related_words   : { type: Array },
   description     : String,
   picture         : String,
   follower_count  : { type: Number, default: 0 },
@@ -21,6 +22,16 @@ TopicSchema.pre('save', function (next) {
 });
 
 TopicSchema.static('filterInputs', function (req_body) {
+  // capitalize first letter of topic name
+  if (req_body.name) {
+    req_body.name = req_body.name.charAt(0).toUpperCase() + req_body.name.slice(1);
+  }
+
+  // convert related_word into array
+  if (req_body.related_words) {
+    req_body.related_words = req_body.related_words.split(',');
+  }
+
   delete req_body.follower_count;
   delete req_body.created_at;
   delete req_body.updated_at;
