3f351f0ee84b0f48aa9b9287849337f3d8e44c55
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-07 23:58:55 +0700

    Update Log schema

--- data/schemas/log.js
+++ data/schemas/log.js
@@ -2,8 +2,18 @@ var LogSchema,
   Schema = require('mongoose').Schema;
 
 LogSchema = new Schema({
-  content   : String,
-  created_at: { type: Date, default: Date.now }
+  author                  : { type: Schema.Types.ObjectId,  ref     : 'User' },
+  edited_answer_author    : { type: Schema.Types.ObjectId,  ref     : 'User' },
+  content                 : { type: String,                 required: true },
+  status: {
+    type: String,
+    enum: [
+      'added', 'edited', 'suggested edit',
+      'accepted suggested edit', 'reverted'
+    ]
+  },
+  reverted_revision : { type: Schema.Types.ObjectId,  ref: 'Log' },
+  created_at        : { type: Date,                   default: Date.now }
 });
 
 module.exports = LogSchema;
