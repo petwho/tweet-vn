e907553dd698a287bb2a523dac28da1f0ac645e6
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-07 18:17:12 +0700

    Move events listener from app_question.js to question.js

--- public/javascripts/app/views/app_question.js
+++ public/javascripts/app/views/app_question.js
@@ -1,14 +1,10 @@
 define([
-  'jquery', 'underscore', 'backbone', 'tinymce',
+  'jquery', 'underscore', 'backbone',
   'collections/questions',
   'views/question',
-], function ($, _, Backbone, tinymce, Questions, QuestionView) {
+], function ($, _, Backbone, Questions, QuestionView) {
   var AppQuestionView = Backbone.View.extend({
     el: '#feed-items',
-
-    events: {
-      'click .fake-input' : 'initEditor'
-    },
 
     initialize: function () {
       this.questions = new Questions;
@@ -16,33 +12,9 @@ define([
       this.questions.fetch();
     },
 
-    loadEditor: function (e) {
-    },
-
     addOne: function (question) {
       var questionView = new QuestionView({ model: question});
       this.$el.append(questionView.render().el);
-    },
-
-    initEditor: function (e) {
-      var fake_editor_id = $(e.currentTarget).data('fake-editor-id');
-
-      tinymce.init({
-        setup: function(editor) {
-          editor.on('init', function () {
-            $(e.currentTarget).hide();
-          });
-        },
-        selector: 'textarea.ec_' + fake_editor_id,
-        plugins: ["autolink autoresize lists link image anchor paste"],
-        toolbar1: "styleselect | bold italic | bullist numlist outdent indent | link image",
-        paste_as_text: true,
-        menubar:false,
-        statusbar: false,
-        min_height: 50,
-        autoresize_min_height: 50,
-        autoresize_bottom_margin: 10
-      });
     }
   });
 
