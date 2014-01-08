b24fcc235f87bc41fe0d1d28f13f9b98822af8de
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-08 14:33:23 +0700

    Implement create answer in route

--- routes/answers.js
+++ routes/answers.js
@@ -1,28 +1,72 @@
 var loggedIn  = require('./middleware/logged_in'),
-  // async       = require('async'),
+  async       = require('async'),
   Question    = require('../data/models/question'),
   Answer      = require('../data/models/answer'),
+  Log         = require('../data/models/log'),
   User        = require('../data/models/user');
 
 module.exports = function (app) {
   app.post('/answers', [loggedIn], function (req, res, next) {
-    var create_log, create_answer, save_answer_log, save_question_log;
+    var create_answer,    create_log,
+      add_log_to_answer,  validate_question,  update_question_log_and_answer,
+      question,           answer,             log;
+
+    log = {};
+
+    validate_question = function (next) {
+      Question.findById(req.body.question, function (err, returned_question) {
+        if (err) { return next(err); }
+        if (!returned_question) { return res.json(400, { msg: 'Invalid question' }); }
+        question = returned_question;
+        next();
+      });
+    };
+
+    create_answer = function (next) {
+      Answer.filterInputs(req.body);
+      req.body.author = req.session.user._id;
+      Answer.create(req.body, function (err, returned_answer) {
+        if (err) { return next(err); }
+        answer = returned_answer;
+        next();
+      });
+    };
 
     create_log = function (next) {
-      var log;
-      log = req.content;
+      log.user = req.session.user._id;
+      log.answer = answer;
+      log.content = req.body.content;
+      log.status = 1;
+
+      Log.create(log, function (err, returned_log) {
+        if (err) { return next(err); }
+        log = returned_log;
+        next();
+      });
     };
-    Answer.filterInputs(req.body);
 
-    req.body.author = req.session.user._id;
+    add_log_to_answer = function (next) {
+      answer.logs = [log];
+      answer.save(function (err, answer, number_affected) {
+        if (err) { return next(err); }
+        next();
+      });
+    };
 
+    update_question_log_and_answer = function (next) {
+      question.logs.push(log);
+      question.answers.push(answer);
+      question.save(function (err, question) {
+        if (err) { return next(err); }
+        next();
+      });
+    };
 
-
-
-    Answer.create(req.body, function (err, answer) {
+    async.series([
+      validate_question, create_answer, create_log, add_log_to_answer, update_question_log_and_answer
+    ], function (err, results) {
       if (err) { return next(err); }
-
-      return res.json(200, answer);
+      res.json(200, answer);
     });
   });
 };
