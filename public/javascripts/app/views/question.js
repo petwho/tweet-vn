f552e9be1c9adde4f7bc211cca98788d2a103f17
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-08 17:57:20 +0700

    Change Backbone template file

--- public/javascripts/app/views/question.js
+++ public/javascripts/app/views/question.js
@@ -8,32 +8,31 @@ define([
 ], function (Backbone, spinner, tinymce, questionTemplate, skinCSS, contentCSS, contentInlineCSS) {
   var QuestionView = Backbone.View.extend({
 
-    template: _.template($(questionTemplate).html()),
+    template: _.template(questionTemplate),
 
     events: {
-      'click .fake-editor' : 'initEditor'
+      'click .fake-editor'  : 'initEditor',
+      'click .cancel-btn'   : 'cancel',
+      'click .submit-btn'   : 'submit'
     },
 
-    afterRender: function () {
-      this.$fakeEditor    = this.$('.fake-editor');
-      this.$fakeInput     = this.$('.fake-editor input');
-      this.$inlineAnswer  = this.$('.inline-answer');
-      this.$answerText    = this.$('.answer-text');
-      this.oldInputText   = this.$fakeInput.val();
-      this.data_editor    = this.$fakeEditor.data('editor');
+    hideFakeEditor: function (self) {
+      self.$inlineAnswer.addClass('wide');
+
+      self.$fakeEditor.hide();
+      self.$fakeInput.val(self.oldInputText);
     },
 
     onEditorInit: function (self, editor) {
+      self.editor = editor;
+
       if ($('head style[name="tinymce"]').length === 0) {
         $('head').append('<style name="tinymce">' + skinCSS + '</style>');
       }
       tinyMCE.activeEditor.dom.add(tinyMCE.activeEditor.dom.select('head'), 'style', { type : 'text/css' }, contentCSS);
       tinyMCE.activeEditor.dom.add(tinyMCE.activeEditor.dom.select('head'), 'style', { type : 'text/css' }, contentInlineCSS);
 
-      self.$inlineAnswer.addClass('wide');
-
-      self.$fakeEditor.hide();
-      self.$fakeInput.val(this.oldInputText);
+      self.hideFakeEditor(self);
 
       self.$answerText.removeClass('hidden');
     },
@@ -42,6 +41,12 @@ define([
       var that = this;
 
       this.$fakeInput.val('Loading...');
+
+      if (this.editor) {
+        this.hideFakeEditor(this);
+        this.$answerText.removeClass('hidden');
+        return;
+      }
 
       tinymce.init({
         setup: function (editor) {
@@ -62,11 +67,30 @@ define([
       });
     },
 
+    cancel: function () {
+      this.$answerText.addClass('hidden');
+      this.$inlineAnswer.removeClass('wide');
+      this.$fakeEditor.show();
+    },
+
+    submit: function () {
+      this.model.trigger('add:answer', this);
+    },
+
     render: function () {
       this.$el.html(this.template(this.model.toJSON()));
       this.$('.fake-editor img').attr({ src: $('.user-picture').attr('src') });
       this.afterRender();
       return this;
+    },
+
+    afterRender: function () {
+      this.$fakeEditor    = this.$('.fake-editor');
+      this.$fakeInput     = this.$('.fake-editor input');
+      this.$inlineAnswer  = this.$('.inline-answer');
+      this.$answerText    = this.$('.answer-text');
+      this.oldInputText   = this.$fakeInput.val();
+      this.data_editor    = this.$fakeEditor.data('editor');
     }
   });
 
