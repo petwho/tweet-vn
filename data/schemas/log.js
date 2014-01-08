a1a6f612f9db208fb08d7177dd3ac5716dc80436
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-08 11:04:39 +0700

    Update Log schema

--- data/schemas/log.js
+++ data/schemas/log.js
@@ -2,18 +2,21 @@ var LogSchema,
   Schema = require('mongoose').Schema;
 
 LogSchema = new Schema({
-  author                  : { type: Schema.Types.ObjectId,  ref     : 'User' },
-  edited_answer_author    : { type: Schema.Types.ObjectId,  ref     : 'User' },
+  user                    : { type: Schema.Types.ObjectId,  ref: 'User' },
+  question                : { type: Schema.Types.ObjectId,  ref: 'Question',  sparse: true },
+  answer                  : { type: Schema.Types.ObjectId,  ref: 'Answer',    sparse: true },
+  reverted_by_revision    : { type: Schema.Types.ObjectId,  ref: 'Log',       sparse: true },
+  revert_of_revision      : { type: Schema.Types.ObjectId,  ref: 'Log',       sparse: true },
   content                 : { type: String,                 required: true },
-  status: {
-    type: String,
-    enum: [
-      'added', 'edited', 'suggested edit',
-      'accepted suggested edit', 'reverted'
-    ]
-  },
-  reverted_revision : { type: Schema.Types.ObjectId,  ref: 'Log' },
-  created_at        : { type: Date,                   default: Date.now }
+  // status code description
+  //  * 1: added      - (for both)
+  //  * 2: edited     - (for question) or suggested edits (for answer)
+  //  * 3: reverted   - (for both: editor revert their own edits)
+  //  * 4: accepted   - (for answer: suggested answer edits)
+  //  * 5: discarded  - (for answer: discarded suggested edits)
+  status              : { type: Number },
+  created_at          : { type: Date, default: Date.now },
+  updated_at          : { type: Date, default: Date.now },
 });
 
 module.exports = LogSchema;
