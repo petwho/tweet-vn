89e59b1a40fb71ad3919ae442845f8892cdcb506
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-12 10:54:39 +0700

    Refine Activity and Comment schemas

--- data/schemas/activity.js
+++ data/schemas/activity.js
@@ -1,22 +1,23 @@
-var Schema        = require('mongoose').Schema,
-  AnswerSchema    = require('answer'),
-  UserSchema      = require('user'),
-  QuestionSchema  = require('question'),
-  TopicSchema     = require('topic'),
-  VoteSchema      = require('vote'),
-  CommentSchema   = require('comment');
+var Schema        = require('mongoose').Schema;
 
 var ActivitySchema = new Schema({
-  is_hidden: { type: Boolean, required: true, default: false },
-  vote: [VoteSchema], // * contain a single document
-  post: {
-    question  : [QuestionSchema], // * contain a single document
-    answer    : [AnswerSchema],   // * contain a single document
-    comment   : [CommentSchema]   // * contain a single document
+  is_hidden : { type: Boolean, required: true, default: false },
+  // 10: vote answer,
+  // 20: post question, 21, post answer, 22: post comment
+  // 30: follow user, 31: follow question, 32: follow topic
+  type    : { type: Number, required: true }
+  vote_id : { type: Schema.Types.ObjectId, ref: 'Vote', sparse: true },
+  post    : {
+    question_id  : { type: Schema.Types.ObjectId, ref: 'Question', sparse: true },
+    answer_id    : { type: Schema.Types.ObjectId, ref: 'Answer', sparse: true },
+    comment_id   : { type: Schema.Types.ObjectId, ref: 'Comment', sparse: true }
   },
-  // * all keys within follow field contain a single document
-  follow      : { user: [UserSchema],  question: [QuestionSchema], topic: [TopicSchema] },
-  created_at  : { type: Date,        default : Date.now }
+  follow: {
+    user_id     : { type: Schema.Types.ObjectId, ref: 'User', sparse: true },
+    question_id : { type: Schema.Types.ObjectId, ref: 'Question', sparse: true },
+    topic_id    : { type: Schema.Types.ObjectId, ref: 'Topic', sparse: true }
+  },
+  created_at: { type: Date,        default : Date.now }
 });
 
 module.exports = ActivitySchema;
