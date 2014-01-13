3aa9e6caa7ccf8b248bdc8fc34f09db4ce53198b
trankhanhsvn <trankhanhsvn@gmail.com>
2014-01-13 11:29:04 +0700

    - Add NewsFeed and Quetions_Answers schemas - Update Activity schema

--- data/schemas/activity.js
+++ data/schemas/activity.js
@@ -1,23 +1,28 @@
-var Schema        = require('mongoose').Schema;
+var Schema        = require('mongoose').Schema,
+  Vote = require('./vote'),
+  Question = require('./question'),
+  Answer = require('./answer'),
+  Topic = require('./topic'),
+  Comment = require('/comment');
 
 var ActivitySchema = new Schema({
   is_hidden : { type: Boolean, required: true, default: false },
   // 10: vote answer,
   // 20: post question, 21, post answer, 22: post comment
   // 30: follow user, 31: follow question, 32: follow topic
-  type    : { type: Number, required: true }
-  vote_id : { type: Schema.Types.ObjectId, ref: 'Vote', sparse: true },
-  post    : {
-    question_id  : { type: Schema.Types.ObjectId, ref: 'Question', sparse: true },
-    answer_id    : { type: Schema.Types.ObjectId, ref: 'Answer', sparse: true },
-    comment_id   : { type: Schema.Types.ObjectId, ref: 'Comment', sparse: true }
+  type      : { type: Number, required: true }
+  voted     : [Vote], // single doc
+  posted    : {
+    question  : [Question], // single doc
+    answer    : [Answer], // single doc
+    comment   : [Comment] // single doc
   },
-  follow: {
-    user_id     : { type: Schema.Types.ObjectId, ref: 'User', sparse: true },
-    question_id : { type: Schema.Types.ObjectId, ref: 'Question', sparse: true },
-    topic_id    : { type: Schema.Types.ObjectId, ref: 'Topic', sparse: true }
+  followed: {
+    user     : [User], // single doc
+    question : [Question], // single doc
+    topic    : [Topic] // single doc
   },
-  created_at: { type: Date,        default : Date.now }
+  created_at: { type: Date, default : Date.now }
 });
 
 module.exports = ActivitySchema;
